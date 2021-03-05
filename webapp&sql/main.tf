5260c9eddaf23d27bbaa3a36872904f71a7e14b5
Onyekachukwu Gibson Ekezie <57141168+goekezie@users.noreply.github.com>
2021-03-05 19:54:13 +0100

    Create main.tf

---
+++ webapp&sql/main.tf
@@ -0,0 +1,112 @@
+# Configure the Azure provider
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "partsunlimited"
+  location = "westus2"
+}
+
+resource "azurerm_key_vault" "example" {
+  name                       = "examplekeyvault"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "create",
+      "get",
+    ]
+
+    secret_permissions = [
+      "set",
+      "get",
+      "delete",
+      "purge",
+      "recover"
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "Admin1234567"
+  value        = "Admin1234567"
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+
+
+resource "azurerm_sql_server" "example" {
+  name                = "parts4unlimited"
+  location            = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "Admin1234567"
+  administrator_login_password = "Admin1234567"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "parts4unlimted667"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_sql_database" "example" {
+  name                = "partsunlimited"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.example.name
+  location            = azurerm_resource_group.rg.location
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "parts4unlimitedplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  count               = 2  
+  name                = element(var.app_service_name, count.index)
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    
+  }
+
+#   app_settings = {
+#     "SOME_KEY" = "some-value"
+#   }
+
+#   connection_string {
+#     name  = "Database"
+#     type  = "SQLServer"
+#     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+#   }
+}
+
