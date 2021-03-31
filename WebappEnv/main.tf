7a4e85be6ed495ceed2065963905c3641073aae0
Onyeka <drealgibson@gmail.com>
2021-03-31 17:02:52 +0100

    added element to name value in main.tf

--- WebappEnv/main.tf
+++ WebappEnv/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_app_service_plan" "example" {
 
 resource "azurerm_app_service" "example" {  
   count               = var.count_num 
-  name                = var.app_service_name
+  name                = element(var.app_service_name, count.index)
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.example.id
