214f6f983f98bdfd12220bba7d6c8db45c000038
Onyeka <drealgibson@gmail.com>
2021-04-05 14:58:56 +0100

    edited main.tf for webapps to include already created rg

---
+++ Webapps/mainexistingrg.tf
@@ -0,0 +1,45 @@
+provider "azurerm" {
+  features {}
+}
+
+# resource "azurerm_resource_group" "rg" {
+#   name     = "tailspin-space-game-rg"
+#   location = "westus2"
+# }
+
+#if resource group already exists
+
+data "azurerm_resource_group" "rg" {
+  name = "tailspin-space-game-rg"
+}
+
+output "id" {
+  value = data.azurerm_resource_group.rg.id
+}
+
+resource "azurerm_app_service_plan" "example" {
+  count               = 2 
+  name                = element(var.app_service_plan_name, count.index)
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  sku {
+    tier = element(var.sku_tier, count.index)
+    size = element(var.sku_size, count.index)
+  }
+}
+
+resource "azurerm_app_service" "dev" {  
+  count               = 2 
+  name                = element(var.app_service_name_dev, count.index)
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.example[0].id
+}
+
+resource "azurerm_app_service" "prod" {  
+  name                = var.app_service_name_prod
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.example[1].id
+}
